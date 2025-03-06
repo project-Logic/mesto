e6ad57d2bcecceda912e2c0609ffcbe33f648448
project-Logic <groshev.sergey.r@gmail.com>
2025-03-06 21:31:45 +0300

    added microfrontends

--- frontend/microfrontend/authorizer/components/App.js
+++ frontend/microfrontend/authorizer/components/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Route, useHistory, Switch } from "react-router-dom";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
-import PopupWithForm from "./PopupWithForm";
-import ImagePopup from "./ImagePopup";
-import api from "../utils/api";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
-import EditProfilePopup from "./EditProfilePopup";
-import EditAvatarPopup from "./EditAvatarPopup";
-import AddPlacePopup from "./AddPlacePopup";
-import Register from "./Register";
-import Login from "./Login";
-import InfoTooltip from "./InfoTooltip";
-import ProtectedRoute from "./ProtectedRoute";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Footer from "./Footer.js";
+import PopupWithForm from "./PopupWithForm.js";
+import ImagePopup from "./ImagePopup.js";
+import api from "../../../src/utils/api.js";
+import { CurrentUserContext } from "../../shared/src/components/CurrentUserContext.js";
+import EditProfilePopup from "./EditProfilePopup.js";
+import EditAvatarPopup from "./EditAvatarPopup.js";
+import AddPlacePopup from "./AddPlacePopup.js";
+import Register from "./Register.js";
+import Login from "./Login.js";
+import InfoTooltip from "../../authorizer/src/components/InfoTooltip.js";
+import ProtectedRoute from "../../authorizer/src/components/ProtectedRoute.js";
 import * as auth from "../utils/auth.js";
 
 function App() {
